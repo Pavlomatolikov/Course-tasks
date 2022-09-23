3269b4296ddd1f06df77a243106783ea54ee039a
Pavlomatolikov <Pavlomatolikov@gmail.com>
2022-09-23 21:25:40 +0200

    Bug with now date is fixed

--- hw_mod8/mod8.py
+++ hw_mod8/mod8.py
@@ -2,28 +2,26 @@ from collections import defaultdict
 from datetime import datetime, timedelta
 from locale import currency
 
-# user_list = [{'name': 'John', 'birthday': datetime(
-#     year=2012, month=9, day=17)}, {'name': 'John', 'birthday': datetime(
-#         year=2012, month=9, day=17)}]
+user_list = [{'name': 'John', 'birthday': datetime(
+    year=2012, month=9, day=24)}, {'name': 'John', 'birthday': datetime(
+        year=2012, month=9, day=17)}]
 
 
 def get_birthdays_per_week(users):
-    # today = datetime.now().date()
-    today = datetime(
-        year=2022, month=9, day=19).date()
+    today = datetime.now().date()
     current_year = today.year
     current_week_number = today.isocalendar().week
     result = defaultdict(list)
-    for i in users:
-        birthday = i['birthday'].date().replace(year=current_year)
+    for user in users:
+        birthday = user['birthday'].date().replace(year=current_year)
         day_name = birthday.strftime('%A')
-        delta = i['birthday'].date().replace(year=current_year) - today
+        delta = user['birthday'].date().replace(year=current_year) - today
         if birthday.isocalendar().week == current_week_number and birthday.weekday() not in (5, 6) and delta.days >= 0:
-            result[day_name].append(i['name'])
+            result[day_name].append(user['name'])
         elif birthday.isocalendar().week - current_week_number == -1 and birthday.weekday() in (5, 6) and today.weekday() == 0:
-            result['Passed ' + day_name].append(i['name'])
-        for key in result:
-            print(f"{key}:", ", ".join(result[key]))
+            result['Passed ' + day_name].append(user['name'])
+    for key in result:
+        print(f"{key}:", ", ".join(result[key]))
 
 
-# print(get_birthdays_per_week(user_list))
+get_birthdays_per_week(user_list)
